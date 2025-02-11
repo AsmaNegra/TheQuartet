279270d3d6b42ece64523c5994f4870db99ccf17
Asma <asma.negra@esprit.tn>
2025-02-11 09:54:17 +0100

    ...

---
+++ src/main/java/controllers/HomePage.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import com.sun.javafx.FXPermissions;
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+
+public class HomePage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load
+        }
+    }
+}
