d0d82b79f936fdd678e8fb0cdfe24566876fcb80
mariemmanai <125659116+mariemmanai@users.noreply.github.com>
2025-02-11 09:59:15 +0100

    a

---
+++ src/main/java/controllers/HomePage.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+
+import java.io.IOException;
+
+public class HomePage extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/Inscription.fxml"));
+        } catch (IOException e){
+                System.out.println(e.getMessage());}
+        }
+
+    }
+}
