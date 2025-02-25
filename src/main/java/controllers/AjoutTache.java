28295cfd642e7248da352435c27e20e2f0d30e72
nourbellaJ0 <165608461+nourbellaJ0@users.noreply.github.com>
2025-02-25 23:11:53 +0100

    FINAL

--- src/main/java/controllers/AjoutTache.java
+++ src/main/java/controllers/AjoutTache.java
@@ -3,6 +3,9 @@ package controllers;
 import entities.Tache;
 import entities.Utilisateur;
 import entities.Fournisseur;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -13,10 +16,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.input.Dragboard;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.input.TransferMode;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import services.ServiceEvenement;
 import services.ServiceTache;
 import services.ServiceUtilisateurEvenement;
@@ -32,6 +38,24 @@ import java.util.List;
 import java.util.ResourceBundle;
 
 public class AjoutTache implements Initializable {
+    //MENU ATTRIBUTS//
+    @FXML
+    public Button btnLogout;
+    @FXML
+    private Button btnSitemap;
+    @FXML
+    private Button btnGift;
+    @FXML
+    private Button btnHome;
+    @FXML
+    private Button btnHome1;
+
+    @FXML
+    private AnchorPane sidebar;
+
+    @FXML
+    private Label labelUser;
+    /////////////////
 
     @FXML
     private DatePicker dateLimitePicker;
@@ -256,4 +280,91 @@ public class AjoutTache implements Initializable {
             e.printStackTrace();
         }
     }
+    //////////////////////MENU/////////////////////////////////
+    @FXML
+    void expandSidebar(MouseEvent event) {
+        // Animate sidebar expansion (e.g., from 70 to 200 pixels)
+        Timeline expandTimeline = new Timeline();
+        KeyValue widthValue = new KeyValue(sidebar.prefWidthProperty(), 200);
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(300), widthValue);
+        expandTimeline.getKeyFrames().add(keyFrame);
+        expandTimeline.play();
+
+        // Set the text for each button
+        btnSitemap.setText("Mes evenements");
+        btnGift.setText("Admin");
+        btnHome.setText("Tous les evenements");
+        btnHome1.setText("My Account");
+        btnLogout.setText("Logout");
+        labelUser.setVisible(true);
+    }
+
+    @FXML
+    void collapseSidebar(MouseEvent event) {
+        // Animate sidebar collapse (e.g., back to 70 pixels)
+        Timeline collapseTimeline = new Timeline();
+        KeyValue widthValue = new KeyValue(sidebar.prefWidthProperty(), 70);
+        KeyFrame keyFrame = new KeyFrame(Duration.millis(300), widthValue);
+        collapseTimeline.getKeyFrames().add(keyFrame);
+        collapseTimeline.play();
+
+        // Clear the text for each button
+        btnSitemap.setText("");
+        btnGift.setText("");
+        btnHome.setText("");
+        btnHome1.setText("");
+        btnLogout.setText("");
+        labelUser.setVisible(false);
+    }
+
+
+
+    @FXML
+    void handleGiftClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/AdminFournisseur.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    void handleHomeClick(ActionEvent event) {
+        try {
+
+            // FXMLLoader loader = new FXMLLoader(getClass().getResource("/Ref.fxml"));
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ViewAllEvents.fxml"));
+
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @FXML
+    void handleSitemapClick(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/EventOrganisation.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void handleLogoutClick(ActionEvent event) {
+
+    }
+    //////////////////////////////////////////////////////////
 }
